811e88803387d54ca3127cf985cba863335c3c8e
Naveen-0112 <4ukumar.g@gmail.com>
2020-09-15 14:27:24 +0530

    VSS-change-code

--- src/com/app/test/SampleTestJava.java
+++ src/com/app/test/SampleTestJava.java
@@ -5,7 +5,7 @@ public class SampleTestJava {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("welcome to GITHUB!!!!!!");
+		int a=10;
 		
 		System.out.println("u sai nagar sai anna Dolle ka Sab!!!!!!");
 	}
